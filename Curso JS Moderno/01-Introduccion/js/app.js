232b9efa546f8aa814288036e0229a95981c3c68
ViridianaSolis <69129000+DianaSOLher@users.noreply.github.com>
2025-03-16 18:56:33 -0600

    1-7

---
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,26 @@
+//Pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript`;
+
+//Asigna el valor hacia la variable del productp
+ const producto = 'Monitor 24 pulgadas';
+
+const hola = "Hola desde la consola";
+console.log(hola);
+console.table([1,2,3,5]);
+
+console.error('Algo salio mal');
+
+console.time("Hola");
+
+console.warn('eSO NO ESTA PERMITIRDO');
+console.warn('eSO NO ESTA PERMITIRDO');
+console.warn('eSO NO ESTA PERMITIRDO');
+console.warn('eSO NO ESTA PERMITIRDO');
+console.warn('eSO NO ESTA PERMITIRDO');
+console.warn('eSO NO ESTA PERMITIRDO');
+
+console.time("Hola");
+
---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,28 @@
+//Inicializar variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+// JS es un lenguaje de tipo dinamico , no especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+//  Se purfr inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+disponible = false;
+
+// Inicializar multiples variables
+
+var categoria = 'Computadoras',
+marca = "Marca famosa",
+calificacion = 5;
+
+//Las variables no pueden iniciar con numeros
+
+var dias99;
+var 91_baabd;
+var _hola;
+
+var holaComoEstas;
+var nombre_producto_categoria_precio;
+var nombre producto;
